a83b778cb8cbf37bb5d51951373a14a23faba6eb
Nguyen Cong Dat <traceformula2000@gmail.com>
2020-06-28 11:00:11 +0700

    Develop | Sorting | Implement Insertion sort

--- sorting/python/insertion_sort.py
+++ sorting/python/insertion_sort.py
@@ -1,3 +1,13 @@
 def sort(array):
-    # TODO: Implement sorting
+    for sortedSize in range(len(array)):
+        insertedValue = array[sortedSize]
+        indexToInsert = sortedSize - 1
+        while indexToInsert >= 0:
+            if array[indexToInsert] > insertedValue:
+                array[indexToInsert + 1] = array[indexToInsert]
+            else:
+                break
+            indexToInsert -= 1
+        indexToInsert += 1
+        array[indexToInsert] = insertedValue
     return array

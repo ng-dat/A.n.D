19e1314d09b0f168233cdaf7a9214960e2f7aaf2
Nguyen Cong Dat <traceformula2000@gmail.com>
2020-06-28 10:48:54 +0700

    Develop | Sorting | implement bubble sort

--- sorting/python/bubble_sort.py
+++ sorting/python/bubble_sort.py
@@ -1,3 +1,10 @@
 def sort(array):
-    # TODO: Implement sorting
+    for sortedSize in range(0, len(array) - 1):
+        swapped = False
+        for i in range(0, len(array) - 1 - sortedSize):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
+        if not swapped:
+            break
     return array

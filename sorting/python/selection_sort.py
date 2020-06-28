294e5e054c67a511dab784b5bb1e80c9c697997e
Nguyen Cong Dat <traceformula2000@gmail.com>
2020-06-28 10:49:18 +0700

    Develop | Sorting | Implement selection sort

--- sorting/python/selection_sort.py
+++ sorting/python/selection_sort.py
@@ -1,3 +1,8 @@
 def sort(array):
-    # TODO: Implement sorting
+    for sortedSize in range(len(array) - 1):
+        minIndex = sortedSize
+        for i in range(sortedSize, len(array)):
+            if array[i] < array[minIndex]:
+                minIndex = i
+        array[sortedSize], array[minIndex] = array[minIndex], array[sortedSize]
     return array

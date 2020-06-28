053d7b685d5eae36bdb9d5e978a16d1056bbfc1c
Nguyen Cong Dat <traceformula2000@gmail.com>
2020-06-28 12:21:43 +0700

    Develop | Sorting | Implement Merge Sort

--- sorting/python/merge_sort.py
+++ sorting/python/merge_sort.py
@@ -1,3 +1,21 @@
 def sort(array):
-    # TODO: Implement sorting
-    return array
+    def _merge(leftArray, rightArray):
+        array = []
+        while leftArray and rightArray:
+            if leftArray[0] < rightArray[0]:
+                array.append(leftArray.pop(0))
+            else:
+                array.append(rightArray.pop(0))
+        array.extend(leftArray)
+        array.extend(rightArray)
+        return array
+
+    if len(array) <= 1:
+        return array
+
+    midIndex = int((len(array) + 1) / 2)
+
+    leftArray = sort(array[:midIndex])
+    rightArray = sort(array[midIndex:])
+
+    return _merge(leftArray, rightArray)
